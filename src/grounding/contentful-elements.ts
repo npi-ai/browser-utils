bc97add4c1a386722eafb671e984f0495e915dd8
Daofeng Wu <dolphin.w.e@gmail.com>
2024-10-18 12:26:57 +0900

    refactor(contentful-elements): filter out invisible items

--- src/grounding/contentful-elements.ts
+++ src/grounding/contentful-elements.ts
@@ -1,35 +1,47 @@
 export function isContentlessEl(el: Element) {
-  return /^body|head|html|script|noscript|style|select|form$/i.test(el.tagName);
+  if (/^body|head|html|script|noscript|style|select|form$/i.test(el.tagName)) {
+    return true;
+  }
+
+  const rect = el.getBoundingClientRect();
+
+  return rect.width === 0 || rect.height === 0;
 }
 
 export function getMostContentfulElements(
   topN: number = 3,
 ): [Element, number][] {
-  const childrenCountMap = new Map();
+  const childrenCountMap = new Map<Element, number>();
 
   [...document.all].forEach(el => {
     if (isContentlessEl(el)) {
       return;
     }
 
-    let count = 0;
+    const tagCount = new Map<string, number>();
 
     for (const child of el.children) {
       if (isContentlessEl(child)) {
         continue;
       }
 
+      const tagName = child.tagName;
       const textContent = child.textContent;
 
       if (
         child.querySelector('a[href], img[src]') ||
         (textContent && textContent.length > 20)
       ) {
-        count++;
+        tagCount.set(tagName, (tagCount.get(tagName) || 0) + 1);
       }
     }
 
-    childrenCountMap.set(el, count);
+    // get most common tag
+    const mostCommonTag = [...tagCount.entries()].sort(
+      (a, b) => b[1] - a[1],
+    )[0];
+
+    childrenCountMap.set(el, mostCommonTag ? mostCommonTag[1] : 0);
   });
 
   // sort by children count
